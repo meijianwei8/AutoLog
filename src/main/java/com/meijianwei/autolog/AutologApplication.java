2347044e0651453c2a2e3360d371a53afe49afd0
meijianwei <574116351@qq.com>
2018-11-26 01:10:24 +0800

    Project Create

---
+++ src/main/java/com/meijianwei/autolog/AutologApplication.java
@@ -0,0 +1,12 @@
+package com.meijianwei.autolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutologApplication.class, args);
+    }
+}
